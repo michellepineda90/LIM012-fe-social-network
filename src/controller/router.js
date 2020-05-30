fee7ca769e8af2a5aa089bdd0ce88964d9d34cd0
Elena <karengvcs@gmail.com>
2020-05-30 01:36:10 -0500

    Update routes

--- src/controller/router.js
+++ src/controller/router.js
@@ -1,22 +1,28 @@
+
 import { components } from '../view/index.js';
+import { control } from './index.controller.js';
 
 export const changeTmp = (hash) => {
-  const id = hash.split('/')[1];
+  // const id = hash.split('/')[1];
   const sectionMain = document.getElementById('container');
   sectionMain.innerHTML = '';
 
   switch (hash) {
-    case '':
-    case '#':
-    case '#/':
-    { return sectionMain.appendChild(components.home()); }
     case '#/login':
-    { return sectionMain.appendChild(components.login()); }
+      sectionMain.appendChild(components.login());
+      control.signInUser();
+      break;
     case '#/register':
-    { return sectionMain.appendChild(components.register()); }
+      sectionMain.appendChild(components.register());
+      control.signUpUser();
+      break;
     case '#/profile':
-    { return sectionMain.appendChild(components.profile()); }
+      sectionMain.appendChild(components.profile());
+      break;
+    case '#/home':
+      sectionMain.appendChild(components.home());
+      break;
     default:
-      return sectionMain.appendChild(components.different());
+      sectionMain.appendChild(components.login());
   }
 };
