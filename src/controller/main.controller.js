46de754842f7c1d6054d6dafe8933c1632ee6f8c
Elena <karengvcs@gmail.com>
2020-05-30 01:29:15 -0500

    Create an object that is the main controller
    This object called control defines methods linked to other controllers

---
+++ src/controller/main.controller.js
@@ -0,0 +1,8 @@
+import signInController from './signInController.js';
+import signUpController from './signUpController.js';
+
+const control = {
+  signInUser: signInController,
+  signUpUser: signUpController,
+};
+export { control };
