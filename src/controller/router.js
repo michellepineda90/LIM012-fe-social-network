5f8b6a7b20eb0714bcb11da4cfd8ba402175b8ac
RLCHuncp <rlch06.uncp@gmail.com>
2020-06-01 12:25:37 -0500

    Posicionar login al abrir la página web

--- src/controller/router.js
+++ src/controller/router.js
@@ -1,7 +1,7 @@
 import { components } from '../view/index.js';
 
 export const changeTmp = (hash) => {
-  const id = hash.split('/')[1];
+  // const id = hash.split('/')[1];
   const sectionMain = document.getElementById('container');
   sectionMain.innerHTML = '';
 
@@ -9,12 +9,11 @@ export const changeTmp = (hash) => {
     case '':
     case '#':
     case '#/':
-    { return sectionMain.appendChild(components.home()); }
-    case '#/login':
     { return sectionMain.appendChild(components.login()); }
     case '#/register':
     { return sectionMain.appendChild(components.register()); }
     case '#/profile':
+    // Arreglar
     { return sectionMain.appendChild(components.profile()); }
     default:
       return sectionMain.appendChild(components.different());
