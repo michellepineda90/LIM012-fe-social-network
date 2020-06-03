a4a33d290c6e140bc8cb68b5ffbacadaaea4e06d
Elena <karengvcs@gmail.com>
2020-06-02 22:43:29 -0500

    update functions of user model

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,23 +1,21 @@
-/* eslint-disable no-console */
+
 
 import { auth, db } from '../firebaseInit.js';
 
-// const registerUserOnDB = (key, data) => new Promise((res, rej) => {
-//   db.collection('users').doc(key).set(data)
-//     .then(res(true))
-//     .catch(rej(new Error('error to storage on database')));
-// });
-const registerUser = (key, data) => db.collection('users').doc(key).set(data);
 
+export const registerUser = (key, data) => db.collection('users').doc(key).set(data);
 
-// export const createUser = user => new Promise((res, rej) => {
-//   auth.createUserWithEmailAndPassword(user.email, user.password)
-//     .then(cred => registerUserOnDB(cred.user.uid, { name: user.name }))
-//     .then(result => res(result))
-//     .catch(err => rej(err));
-// });
+export const signInUser = user => auth.signInWithEmailAndPassword(user.email, user.password);
 
 export const createUser = user => auth.createUserWithEmailAndPassword(user.email, user.password)
   .then(cred => registerUser(cred.user.uid, { name: user.name }));
 
-export const signInUser = user => auth.signInWithEmailAndPassword(user.email, user.password);
+export const signInWithGoogle = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  return firebase.auth().signInWithPopup(provider);
+};
+
+export const signInWithFacebook = () => {
+  const provider = new firebase.auth.FacebookAuthProvider();
+  return firebase.auth().signInWithPopup(provider);
+};
