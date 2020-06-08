9bccb92b08bca180bbd8adfedc08bc78b488e16e
RLCHuncp <rlch06.uncp@gmail.com>
2020-06-07 19:36:41 -0500

    4 Test

--- test/firebase.spec.js
+++ test/firebase.spec.js
@@ -1,21 +1,32 @@
 import './firebase';
 import {
   signInUser,
+  createUser,
   signInWithGoogle,
   signInWithFacebook,
-} from '../src/model/user.model.js';
+  sendEmail,
+  // registerUser,
+} from '../src/model/user.model';
 
 
-describe('Login', () => {
-  it('Deberia iniciar sesión', () => signInUser('lala@gmail.com', '123456')
+describe('Function signInUser()', () => {
+  it('Deberia iniciar sesión', () => signInUser('lala06@gmail.com', '123456')
     .then((user) => {
-      expect(user.email).toBe('lala@gmail.com');
+      expect(user.isAnonymous).toBe(false);
     }));
 });
-
-// describe(' Function createUser()', (user) => {
-
-// });
+// curren user is:  {
+//   isAnonymous: false,
+//   providerData: [ GoogleAuthProvider { providerId: 'google.com' } ]
+// }
+describe('Function createUser()', () => {
+  it('Debería enviar una mensaje de verificación al usuario que se ha registrado', () => {
+    createUser('lucy@gmail.com', '123456')
+      .then(() => {
+        expect(sendEmail).toBe('REGISTRADO EXITOSAMENTE');
+      });
+  });
+});
 
 describe('Function signInWithGoogle', () => {
   it('Debería iniciar sesión con google', () => signInWithGoogle()
