97f6b47ca1444571018022e86fc06950dca0a705
Elena <karengvcs@gmail.com>
2020-06-08 13:51:34 -0500

    Add new interfaces

--- src/view/email.js
+++ src/view/email.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { auth } from '../firebaseInit.js';
 import { sendEmail } from '../model/user.model.js';
 
@@ -5,25 +6,25 @@ export default () => {
   const divElement = document.createElement('div');
   divElement.classList.add('screen-background');
   divElement.innerHTML = `
-  <div class="welcome">
-  <img src="../img/eco-trans-white.png" alt="chat" class="logo"/>
-  <p class="app-name-text">eco-chat...</p>
+  <div class=" logo-container">
+    <img src="/../../img/eco-trans-white.png" alt="" class="logo">
+    <span class="app-name">eco-chat...</span>
   </div>
-  <div class="holder">
   <div class="form-container">
-  <h2 class="holder-title">¡Gracias por registrarte!</h2>    
-  <p class=""> Hemos enviado un correo de confirmación a <strong>${auth.currentUser.email}</strong></p>
-  <p>Por favor revisa tu correo y haz click en el enlace para verificar la dirección del correo.</p>
-  <button class="form-btn btn-send-email">Reenviar correo de verificación</button>
-  <p><a href="#/login"> Inicia Sesión</a></p>
-  </div>
+    <div class="message-container">
+      <h2 class="message-title">¡Gracias por registrarte!</h2>    
+      <span class="text-deco"> Hemos enviado un correo de confirmación a <strong>${auth.currentUser.email}</strong></span>
+      <span class="text-deco">Por favor revisa tu correo y haz click en el enlace para verificar la dirección del correo.</span>
+      <button class="btn-send-email">Reenviar correo de verificación</button>
+      <span><a href="#/login"> Inicia Sesión</a></span>
+    </div>
   </div>
   `;
 
 
   const btn = divElement.querySelector('button');
   btn.addEventListener('click', () => {
-    console.log('volciendo a enviar email');
+    console.log('volviendo a enviar email');
     sendEmail();
   });
 
