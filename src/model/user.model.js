16b3ca01500afb0e5575753ca33a605a72a9df1d
RLCHuncp <rlch06.uncp@gmail.com>
2020-06-08 17:08:46 -0500

    function currentUser()

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -18,3 +18,14 @@ export const signInWithFacebook = () => {
   const provider = new firebase.auth.FacebookAuthProvider();
   return firebase.auth().signInWithPopup(provider);
 };
+
+export const currentUser = () => {
+  const user = firebase.auth().currentUser;
+  const userData = {
+    id: user.uid,
+    name: (user.displayName === null) ? 'Anonimo' : user.displayName,
+    email: user.email,
+    photo: (user.photoURL === null) ? './img/avatar.png' : user.photoURL,
+  };
+  return userData;
+};
