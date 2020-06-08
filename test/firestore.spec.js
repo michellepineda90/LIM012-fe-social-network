5c88acb4a3043e067f20d0ff30e4360cfdff9171
RLCHuncp <rlch06.uncp@gmail.com>
2020-06-08 16:09:54 -0500

    Archivo para testear funciones de firestore

---
+++ test/firestore.spec.js
@@ -0,0 +1,7 @@
+// import { registerUser } from '../src/model/user.model.js';
+
+// describe('function registerUser()', () => {
+//   it('Debería registrarse en firestore', () => {
+
+//   });
+// });
