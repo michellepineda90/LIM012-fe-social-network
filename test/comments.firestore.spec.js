e33fcfa3d52656201cdec0bf33a1e708b32dfcc9
Michelle Pineda <michelle.pineda.rivas@gmail.com>
2020-06-24 08:45:07 -0600

    addCommentBD test

---
+++ test/comments.firestore.spec.js
@@ -0,0 +1,32 @@
+import './firebase';
+import MockFirebase from 'mock-cloud-firestore';
+
+import {
+  addCommentBD, getCommentsForTest, editCommentBD, deleteCommentBD,
+} from '../src/model/post.model.js';
+
+const fixtureData = {
+  __collection__: {
+    comments: {
+      __doc__: {
+        abc1: {
+          textContent: 'holiholiholi',
+          postId: 'a1b2c3',
+        },
+      },
+    },
+  },
+};
+
+global.firebase = new MockFirebase(fixtureData, { isNaiveSnapshotListenerEnabled: true });
+
+describe('addCommentBD', () => {
+  it('Should add a comment in Firestore DB collection, and check it has been correctly linked to a given post',
+    () => addCommentBD({ textContent: 'holiholiholi', postId: 'a1b2c3' })
+      .then(() => getCommentsForTest())
+      .then((data) => {
+        console.log(data);
+        const result = data.find(comment => comment.textContent === 'holiholiholi');
+        expect(result).toMatchObject({ textContent: 'holiholiholi', postId: 'a1b2c3' });
+      }));
+});
